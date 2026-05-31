b2cae02765a7975bec51b7139af35616fc1fd523
Zack Garza <dzackgarza@gmail.com>
2026-05-31 19:03:03 +0800

    fix: make tauri-plugin-playwright always-on, not a feature flag
    Testing infra is not toggleable. A conditional cfg(feature) means the
    binary you test is not the binary you ship.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,23 +11,17 @@ use std::sync::Mutex;
 pub fn run() {
     let initial_state = config::build_initial_state();
 
-    let mut builder = commands::register_commands(
+    commands::register_commands(
         tauri::Builder::default()
             .plugin(tauri_plugin_log::Builder::default().level(log::LevelFilter::Info).build())
             .plugin(tauri_plugin_shell::init())
             .plugin(tauri_plugin_fs::init())
             .plugin(tauri_plugin_dialog::init())
             .plugin(tauri_plugin_http::init())
+            .plugin(tauri_plugin_playwright::init())
             .manage(Mutex::new(initial_state)),
-    );
-
-    #[cfg(feature = "e2e-testing")]
-    {
-        builder = builder.plugin(tauri_plugin_playwright::init());
-    }
-
-    builder
-        .setup(|_app| Ok(()))
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    )
+    .setup(|_app| Ok(()))
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
